013a0e2f83b3314e759e5ea60f4ad44368141912
dgsmith1988 <mandles_crandles@hotmail.com>
2020-12-05 16:59:58 -0500

    Saving code before a massive overhaul in the sub_cycle and full_cycle generation paradigm

--- ChordScaleGenerator.py
+++ ChordScaleGenerator.py
@@ -1,4 +1,4 @@
-from music21 import stream, chord, meter, pitch
+from music21 import stream, chord, meter, pitch, metadata, layout
 import numpy as np
 import copy
 from enum import Enum
@@ -61,18 +61,31 @@ cycle5matrix[2, 0, :] = cycle5matrix[0, 2, :]
 cycle5matrix[2, 1, :] = cycle5matrix[0, 0, :]
 cycle5matrix[2, 2, :] = cycle5matrix[0, 1, :]
 
+# cycle3matrix = np.zeros((3, 3, 3), dtype=np.int)
+# cycle3matrix[0, 0, :] = [-1, 0, 0]
+# cycle3matrix[0, 1, :] = [0, -1, 0]
+# cycle3matrix[0, 2, :] = [0, 0, -1]
+#
+# cycle3matrix[1, 0, :] = cycle3matrix[0, 2, :]
+# cycle3matrix[1, 1, :] = cycle3matrix[0, 0, :]
+# cycle3matrix[1, 2, :] = cycle3matrix[0, 1, :]
+#
+# cycle3matrix[2, 0, :] = cycle3matrix[0, 1, :]
+# cycle3matrix[2, 1, :] = cycle3matrix[0, 2, :]
+# cycle3matrix[2, 2, :] = cycle3matrix[0, 0, :]
+
 cycle3matrix = np.zeros((3, 3, 3), dtype=np.int)
 cycle3matrix[0, 0, :] = [-1, 0, 0]
 cycle3matrix[0, 1, :] = [0, -1, 0]
 cycle3matrix[0, 2, :] = [0, 0, -1]
 
-cycle3matrix[1, 0, :] = cycle3matrix[0, 2, :]
-cycle3matrix[1, 1, :] = cycle3matrix[0, 0, :]
-cycle3matrix[1, 2, :] = cycle3matrix[0, 1, :]
+cycle3matrix[2, 0, :] = cycle3matrix[0, 2, :]
+cycle3matrix[2, 1, :] = cycle3matrix[0, 0, :]
+cycle3matrix[2, 2, :] = cycle3matrix[0, 1, :]
 
-cycle3matrix[2, 0, :] = cycle3matrix[0, 1, :]
-cycle3matrix[2, 1, :] = cycle3matrix[0, 2, :]
-cycle3matrix[2, 2, :] = cycle3matrix[0, 0, :]
+cycle3matrix[1, 0, :] = cycle3matrix[0, 1, :]
+cycle3matrix[1, 1, :] = cycle3matrix[0, 2, :]
+cycle3matrix[1, 2, :] = cycle3matrix[0, 0, :]
 
 cycle6matrix = np.zeros((3, 3, 3), dtype=np.int)
 cycle6matrix[0, 0, :] = [0, 0, 1]
@@ -93,7 +106,7 @@ class Voicing(Enum):
     Drop2 = 2
 
 
-class Range:
+class NoteRange:
     class ReturnValue(Enum):
         BelowRange = -1
         InRange = 0
@@ -105,41 +118,54 @@ class Range:
 
     def is_pitch_in_range(self, p):
         if p.ps > self.highest_pitch.ps:
-            return Range.ReturnValue.AboveRange
+            return NoteRange.ReturnValue.AboveRange
         elif p.ps < self.lowest_pitch.ps:
-            return Range.ReturnValue.BelowRange
+            return NoteRange.ReturnValue.BelowRange
         else:
-            return Range.ReturnValue.InRange
+            return NoteRange.ReturnValue.InRange
 
 
-# class GuitarRange:
-#     # this class is used to limit the chord voicings to keep them from open strings to the 14th fret
-#     E_string_low = Range(pitch.Pitch('E2'), pitch.Pitch('F#3'))
-#     A_string = Range(pitch.Pitch('A2'), pitch.Pitch('B3'))
-#     D_string = Range(pitch.Pitch('D3'), pitch.Pitch('E4'))
-#     G_string = Range(pitch.Pitch('G3'), pitch.Pitch('A4'))
-#     B_string = Range(pitch.Pitch('B3'), pitch.Pitch('C#5'))
-#     E_string_high = Range(pitch.Pitch('E4'), pitch.Pitch('F#5'))
+class GuitarString(NoteRange):
+    # Todo: Rethink this name for the class
+    class StringNumber(Enum):
+        E_string_low = 6
+        A_string = 5
+        D_string = 4
+        G_string = 3
+        B_string = 2
+        E_string_high = 1
+
+    def __init__(self, lowest_pitch, highest_pitch, number):
+        self.number = number
+        NoteRange.__init__(self, lowest_pitch, highest_pitch)
+
 
 class GuitarRange:
-    # this class is used to limit the chord voicings to keep them from open strings to the 14th fret
-    # the note values here correspond to how guitar is notated as opposed to how it's actually pitched
-    E_string_low = Range(pitch.Pitch('E3'), pitch.Pitch('G4'))
-    A_string = Range(pitch.Pitch('A3'), pitch.Pitch('C5'))
-    D_string = Range(pitch.Pitch('D4'), pitch.Pitch('F5'))
-    G_string = Range(pitch.Pitch('G4'), pitch.Pitch('Bb5'))
-    B_string = Range(pitch.Pitch('B4'), pitch.Pitch('D6'))
-    E_string_high = Range(pitch.Pitch('E5'), pitch.Pitch('G6'))
+    # this class is used to limit the chord voicings to keep them from open strings to the 14th fret. the note values
+    # here correspond to how guitar is notated as opposed to how it's actually pitched to make reading the scores easier
+    E_string_low = GuitarString(pitch.Pitch('E3'), pitch.Pitch('G4'), GuitarString.StringNumber.E_string_low)
+    A_string = GuitarString(pitch.Pitch('A3'), pitch.Pitch('C5'), GuitarString.StringNumber.A_string)
+    D_string = GuitarString(pitch.Pitch('D4'), pitch.Pitch('F5'), GuitarString.StringNumber.D_string)
+    G_string = GuitarString(pitch.Pitch('G4'), pitch.Pitch('Bb5'), GuitarString.StringNumber.G_string)
+    B_string = GuitarString(pitch.Pitch('B4'), pitch.Pitch('D6'), GuitarString.StringNumber.B_string)
+    E_string_high = GuitarString(pitch.Pitch('E5'), pitch.Pitch('G6'), GuitarString.StringNumber.E_string_high)
 
-
-guitar_range = [
-    Range(pitch.Pitch('E2'), pitch.Pitch('F#3')),
-    Range(pitch.Pitch('A2'), pitch.Pitch('B3')),
-    Range(pitch.Pitch('D3'), pitch.Pitch('E4')),
-    Range(pitch.Pitch('G3'), pitch.Pitch('A4')),
-    Range(pitch.Pitch('B3'), pitch.Pitch('C#5')),
-    Range(pitch.Pitch('E4'), pitch.Pitch('F#5'))
-]
+    @staticmethod
+    def get_string(string_number):
+        if string_number < 1 or string_number > 6:
+            raise Exception("index passed needs to be between 1 and 6")
+        elif string_number == GuitarString.StringNumber.E_string_high.value:
+            return GuitarRange.E_string_high
+        elif string_number == GuitarString.StringNumber.B_string.value:
+            return GuitarRange.B_string
+        elif string_number == GuitarString.StringNumber.G_string.value:
+            return GuitarRange.G_string
+        elif string_number == GuitarString.StringNumber.D_string.value:
+            return GuitarRange.D_string
+        elif string_number == GuitarString.StringNumber.A_string.value:
+            return GuitarRange.A_string
+        elif string_number == GuitarString.StringNumber.E_string_low.value:
+            return GuitarRange.E_string_low
 
 
 def transpose_chord_as_list(chord_as_list, interval):
@@ -157,6 +183,22 @@ def apply_row_to_chord(row, chord_as_list, sc):
             chord_as_list[j] = sc.next(chord_as_list[j], 'ascending', row[j])
 
 
+def check_note_ranges_and_transpose(chord_as_list, note_ranges):
+    # check to see if the new chord is within the specified range if ranges are specified
+    # transpose the pitches in the chord up or down an octave so they match accordingly
+    # Todo: consider writing this to be more "intelligent" about how it transposes a chord to an arbitrary range, it
+    #  seems as if the transposeAboveTarget() and transposeBelowTarget() would be functions we could use here
+    for note_range, chord_tone in zip(note_ranges, chord_as_list):
+        is_in_range = note_range.is_pitch_in_range(chord_tone)
+        if is_in_range == NoteRange.ReturnValue.BelowRange:
+            # transpose the chord up an octave
+            transpose_chord_as_list(chord_as_list, 12)
+            break
+        elif is_in_range == NoteRange.ReturnValue.AboveRange:
+            transpose_chord_as_list(chord_as_list, -12)
+            break
+
+
 def generate_drop2_matrix_page(cycle_matrix_page):
     # given that a drop 2 triad has the order of chord tones changed, modify the cycle matrix to adjust accordingly.
     # this equates to swapping the columns as the columns indicate the position of the note in the chord but since it's
@@ -170,8 +212,7 @@ def generate_drop2_matrix_page(cycle_matrix_page):
     return drop2_matrix_page
 
 
-def generate_sub_cycle(starting_chord, cycle_matrix_page, sc):
-    # TODO: Clean up the names of the arguments here
+def generate_sub_cycle(root_scale, starting_chord, cycle_matrix_page, note_ranges=()):
     chord_as_list = list(starting_chord.pitches)
     measure = stream.Measure()
     # make it a 7/4 measure as there are 7 chords to a sub-cycle
@@ -183,32 +224,58 @@ def generate_sub_cycle(starting_chord, cycle_matrix_page, sc):
     # at the moment, the cycle will need to be run twice to generate the remaining six chords
     for i in range(0, 2):
         for row in cycle_matrix_page:
-            apply_row_to_chord(row, chord_as_list, sc)
+            apply_row_to_chord(row, chord_as_list, root_scale)
+            # check to see if the new chord is within the specified range if ranges are specified
+            if len(note_ranges) != 0:
+                check_note_ranges_and_transpose(chord_as_list, note_ranges)
             measure.append(chord.Chord(chord_as_list))
     return measure
 
 
-# def generate_full_cycle(starting_chord, cycle_matrix_page, sc):
-#     # the starting chord is set to the last chord produced by the cycle
-#     full_cycle = stream.Stream()
-#     full_cycle.append(meter.TimeSignature('7/4'))
-#
-#     chord_as_list = list(starting_chord.pitches)
-#
-#     # lets start generating the cycle
-#     full_cycle.append(starting_chord)
-#     # the length of this range could be more generalized to the number of notes in the passed scale
-#     for i in range(0, 20):
-#         # use modular arithmetic here in preparation for expanding to 7th chords
-#         apply_row_to_chord(cycle_matrix_page[i % len(cycle_matrix_page)], chord_as_list, sc)
-#         full_cycle.append(chord.Chord(chord_as_list))
-#
-#     apply_row_to_chord(cycle_matrix_page[20 % len(cycle_matrix_page)], chord_as_list, sc)
-#
-#     return full_cycle, chord.Chord(chord_as_list)
+def generate_sub_cycle_2(root_scale, starting_chord, cycle_matrix_page, note_ranges=()):
+    chord_as_list = list(starting_chord.pitches)
+    measure = stream.Measure()
+    # make it a 7/4 measure as there are 7 chords to a sub-cycle
+    measure.append(meter.TimeSignature('7/4'))
+    # add the first chord before generating the rest of the cycle
+    measure.append(chord.Chord(starting_chord))
+
+    # apply the rules of the cycle matrix to generate the rest of the chords. since we're dealing with diatonic chords
+    # at the moment, the cycle will need to be run twice to generate the remaining six chords
+    for i in range(0, 2):
+        for row in cycle_matrix_page:
+            apply_row_to_chord(row, chord_as_list, root_scale)
+            # check to see if the new chord is within the specified range if ranges are specified
+            if len(note_ranges) != 0:
+                check_note_ranges_and_transpose(chord_as_list, note_ranges)
+            measure.append(chord.Chord(chord_as_list))
+
+    apply_row_to_chord(cycle_matrix_page[6 % len(cycle_matrix_page)], chord_as_list, root_scale)
+
+    return measure, chord.Chord(chord_as_list)
 
 
-def generate_full_cycle(starting_chord, cycle_matrix_page, sc, note_range=()):
+def generate_full_cycle_2(root_scale, starting_chord, cycle_matrix, note_ranges=()):
+    # the starting chord is set to the last chord produced by the cycle
+    full_cycle = stream.Stream()
+
+    # the length of this range could be more generalized to the number of notes in the passed scale
+    seed_chord = starting_chord
+    for i in range(0, 3):
+        sub_cycle, next_chord = generate_sub_cycle_2(root_scale, seed_chord, cycle_matrix[i], note_ranges)
+        # the time signature is already taken care of in the first measure, so remove it from the sub-sequent measures
+        if i > 0:
+            sub_cycle.pop(0)
+        full_cycle.append(sub_cycle)
+        seed_chord = next_chord
+
+    # this was put here as it appears there's a bug in the append() function when adding a measure to a stream where
+    # there's a time signature object already
+    # full_cycle.insert(0, meter.TimeSignature('7/4'))
+    return full_cycle, next_chord
+
+
+def generate_full_cycle(root_scale, starting_chord, cycle_matrix_page, note_ranges=()):
     # the starting chord is set to the last chord produced by the cycle
     full_cycle = stream.Stream()
     full_cycle.append(meter.TimeSignature('7/4'))
@@ -220,58 +287,19 @@ def generate_full_cycle(starting_chord, cycle_matrix_page, sc, note_range=()):
     # the length of this range could be more generalized to the number of notes in the passed scale
     for i in range(0, 20):
         # use modular arithmetic here in preparation for expanding to 7th chords
-        apply_row_to_chord(cycle_matrix_page[i % len(cycle_matrix_page)], chord_as_list, sc)
+        apply_row_to_chord(cycle_matrix_page[i % len(cycle_matrix_page)], chord_as_list, root_scale)
         # check to see if the new chord is within the specified range if ranges are specified
-        if len(note_range) != 0:
-            for string, chord_tone in zip(note_range, chord_as_list):
-                ret = string.is_pitch_in_range(chord_tone)
-                if ret == Range.ReturnValue.BelowRange:
-                    # transpose the chord up an octave
-                    transpose_chord_as_list(chord_as_list, 12)
-                    break
-                elif ret == Range.ReturnValue.AboveRange:
-                    transpose_chord_as_list(chord_as_list, -12)
-                    break
+        if len(note_ranges) != 0:
+            check_note_ranges_and_transpose(chord_as_list, note_ranges)
         # add the chord to the cycle since it should be in the specified range now
         full_cycle.append(chord.Chord(chord_as_list))
 
-    apply_row_to_chord(cycle_matrix_page[20 % len(cycle_matrix_page)], chord_as_list, sc)
+    apply_row_to_chord(cycle_matrix_page[20 % len(cycle_matrix_page)], chord_as_list, root_scale)
 
     return full_cycle, chord.Chord(chord_as_list)
 
 
-# def generate_cycle_pairs2(starting_chord, sc, pair_type, voicing_type=Voicing.Closed):
-#     # Todo: Re-write this to use a reversing scheme to make this even more generic as a pair of cycles really is just
-#     #  one cycle follow the its reverse
-#     cycle_pair = stream.Stream()
-#
-#     if pair_type == "2/7":
-#         first_cycle_matrix_page = cycle2matrix[:, :, 0]
-#         second_cycle_matrix_page = cycle7matrix[:, :, 0]
-#     elif pair_type == "4/5":
-#         first_cycle_matrix_page = cycle4matrix[:, :, 0]
-#         second_cycle_matrix_page = cycle5matrix[:, :, 0]
-#     elif pair_type == "3/6":
-#         first_cycle_matrix_page = cycle3matrix[:, :, 0]
-#         second_cycle_matrix_page = cycle6matrix[:, :, 0]
-#     else:
-#         raise Exception("pair_type passed is an invalid value")
-#
-#     if voicing_type == Voicing.Drop2:
-#         first_cycle_matrix_page = generate_drop2_matrix_page(first_cycle_matrix_page)
-#         second_cycle_matrix_page = generate_drop2_matrix_page(second_cycle_matrix_page)
-#
-#     first_cycle, next_chord = generate_full_cycle(copy.deepcopy(starting_chord), first_cycle_matrix_page, sc)
-#     second_cycle = generate_full_cycle(next_chord, second_cycle_matrix_page, sc)[0]
-#
-#     cycle_pair.append(first_cycle)
-#     cycle_pair.append(second_cycle)
-#
-#     return cycle_pair
-
-
-def generate_cycle_pairs(starting_chord, sc, pair_type, string_ranges=(), voicing_type=Voicing.Closed):
-    # Todo: clean up the variable names here to be more accurate
+def generate_cycle_pairs(root_scale, starting_chord, pair_type, note_ranges=(), voicing_type=Voicing.Closed):
     cycle_pair = stream.Stream()
 
     if pair_type == "2/7":
@@ -290,10 +318,77 @@ def generate_cycle_pairs(starting_chord, sc, pair_type, string_ranges=(), voicin
         first_cycle_matrix_page = generate_drop2_matrix_page(first_cycle_matrix_page)
         second_cycle_matrix_page = generate_drop2_matrix_page(second_cycle_matrix_page)
 
-    first_cycle, next_chord = generate_full_cycle(copy.deepcopy(starting_chord), first_cycle_matrix_page, sc, string_ranges)
-    second_cycle = generate_full_cycle(next_chord, second_cycle_matrix_page, sc, string_ranges)[0]
+    first_cycle, next_chord = generate_full_cycle(root_scale, copy.deepcopy(starting_chord), first_cycle_matrix_page,
+                                                  note_ranges)
+    second_cycle = generate_full_cycle(root_scale, next_chord, second_cycle_matrix_page, note_ranges)[0]
 
+    # add notation to make the score easier to read
+    first_cycle[1].lyric = "First cycle starts"
+    second_cycle[1].lyric = "Second cycle starts"
+
+    # sl = layout.SystemLayout(isNew=True)
+    first_cycle.append(layout.SystemLayout(isNew=True))
     cycle_pair.append(first_cycle)
+    # remove the time signature from the second cycle as it will mess up the formatting since the combined stream is
+    # flattened when returned
+    second_cycle.pop(0)
     cycle_pair.append(second_cycle)
 
     return cycle_pair
+
+
+def generate_cycle_pairs2(root_scale, starting_chord, pair_type, note_ranges=(), voicing_type=Voicing.Closed):
+    cycle_pair = stream.Stream()
+
+    if pair_type == "2/7":
+        first_cycle_matrix = cycle2matrix
+        second_cycle_matrix = cycle7matrix
+    elif pair_type == "4/5":
+        first_cycle_matrix = cycle4matrix
+        second_cycle_matrix = cycle5matrix
+    elif pair_type == "3/6":
+        first_cycle_matrix = cycle3matrix
+        second_cycle_matrix = cycle6matrix
+    else:
+        raise Exception("pair_type passed is an invalid value")
+
+    if voicing_type == Voicing.Drop2:
+        first_cycle_matrix = generate_drop2_matrix_page(first_cycle_matrix)
+        second_cycle_matrix = generate_drop2_matrix_page(second_cycle_matrix)
+
+    first_cycle, next_chord = generate_full_cycle_2(root_scale, copy.deepcopy(starting_chord), first_cycle_matrix,
+                                                    note_ranges)
+    second_cycle = generate_full_cycle_2(root_scale, next_chord, second_cycle_matrix, note_ranges)[0]
+
+    # add notation to make the score easier to read
+    first_cycle[0][1].lyric = "First cycle starts"
+    second_cycle[0][1].lyric = "Second cycle starts"
+
+    # sl = layout.SystemLayout(isNew=True)
+    first_cycle.append(layout.SystemLayout(isNew=True))
+    cycle_pair.append(first_cycle)
+    # remove the time signature from the second cycle as it will mess up the formatting since the combined stream is
+    # flattened when returned
+    second_cycle[0].pop(0)
+    cycle_pair.append(second_cycle)
+
+    return cycle_pair
+
+
+def generate_cycle_pairs_for_all_string_sets(root_scale, tonic, pair_type):
+    for i in range(1, 5):
+        string_set = (GuitarRange.get_string(i+2), GuitarRange.get_string(i+1), GuitarRange.get_string(i))
+        # generate the starting chord for this particular string set
+        triad_root = pitch.Pitch(tonic)
+        triad_root.octave = string_set[0].highest_pitch.octave
+        triad_root.transposeBelowTarget(string_set[0].highest_pitch, inPlace=True)
+        tonic_triad = chord.Chord(root_scale.pitchesFromScaleDegrees([1, 3, 5], triad_root, triad_root.transpose(11)))
+        cycle_pair = generate_cycle_pairs(root_scale, tonic_triad, pair_type, string_set)
+        cycle_pair.metadata = metadata.Metadata()
+        cycle_pair.metadata.title = "Cycle " + pair_type + " Progression in " + root_scale.name + "\nString Set: " + \
+            str(string_set[0].number.value) + "-" + str(string_set[1].number.value) + "-" + \
+            str(string_set[2].number.value) + " - Closed Triads"
+        cycle_pair.metadata.composer = "Graham's Chord Cycle Generation Software"
+        cycle_pair.definesExplicitSystemBreaks = True
+        cycle_pair.show()
+        # cycle_pair.write("MusicXML", "C:\\Temp\\Cycle36_progression3")
--- tests.py
+++ tests.py
@@ -1,6 +1,67 @@
 from ChordScaleGenerator import *
-from music21 import scale, chord, key, stream
+from music21 import scale, chord, key, stream, metadata
 from copy import deepcopy
+
+
+def check_note_ranges_test():
+    string_set = (GuitarRange.A_string, GuitarRange.D_string, GuitarRange.G_string)
+
+    # test case 1: chord is within the specified note ranges
+    test_chord = chord.Chord(['E4', 'G4', 'B4'])
+    chord_as_list = list(deepcopy(test_chord.pitches))
+    check_note_ranges_and_transpose(chord_as_list, string_set)
+    test_1 = stream.Measure()
+    test_1.append(test_chord)
+    test_1.append(chord.Chord(chord_as_list))
+    test_1.show()
+
+    # test case 2: chord is above the specified note ranges,
+    test_chord = chord.Chord(['E5', 'G5', 'B5'])
+    chord_as_list = list(deepcopy(test_chord.pitches))
+    check_note_ranges_and_transpose(chord_as_list, string_set)
+    test_2 = stream.Measure()
+    test_2.append(test_chord)
+    test_2.append(chord.Chord(chord_as_list))
+    test_2.show()
+
+    # test case 3: chord is below the specified note ranges, transpose it up an octave
+    test_chord = chord.Chord(['E3', 'G3', 'B3'])
+    chord_as_list = list(deepcopy(test_chord.pitches))
+    check_note_ranges_and_transpose(chord_as_list, string_set)
+    test_3 = stream.Measure()
+    test_3.append(test_chord)
+    test_3.append(chord.Chord(chord_as_list))
+    test_3.show()
+
+
+def sub_cycle_test():
+    tonic = 'C'
+    sc = scale.MajorScale(tonic)
+    root = sc.pitchFromDegree(1)
+
+    first_chord = chord.Chord([root.transpose(12), root.transpose(4+12), root.transpose(7+12)])
+    sub_cycle = generate_sub_cycle(sc, first_chord, cycle2matrix[0])
+    sub_cycle.show()
+
+    first_chord = chord.Chord([root.transpose(4), root.transpose(7), root.transpose(12)])
+    sub_cycle = generate_sub_cycle(sc, first_chord, cycle2matrix[1])
+    sub_cycle.show()
+
+    first_chord = chord.Chord([root.transpose(7), root.transpose(12), root.transpose(12+4)])
+    sub_cycle = generate_sub_cycle(sc, first_chord, cycle2matrix[2])
+    sub_cycle.show()
+
+
+def full_cycle_test():
+    string_set = (GuitarRange.A_string, GuitarRange.D_string, GuitarRange.G_string)
+
+    tonic = 'C'
+    sc = scale.MajorScale(tonic)
+    root = sc.pitchFromDegree(1)
+    tonic_triad = chord.Chord([root, root.transpose(4), root.transpose(7)])
+
+    cycle = generate_full_cycle(sc, tonic_triad, cycle2matrix[0], string_set)[0]
+    cycle.show()
 
 
 def cycle36_drop2_test():
@@ -8,7 +69,7 @@ def cycle36_drop2_test():
     sc = scale.MajorScale(tonic)
     root = sc.pitchFromDegree(1)
     tonic_triad = chord.Chord([root, root.transpose(7), root.transpose(16)])
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6", voicing_type=Voicing.Drop2)
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6", voicing_type=Voicing.Drop2)
     cycle.show()
 
 
@@ -17,7 +78,7 @@ def cycle36_drop3_test():
     sc = scale.MajorScale(tonic)
     root = sc.pitchFromDegree(1)
     tonic_triad = chord.Chord([root, root.transpose(16), root.transpose(20)])
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
     cycle.show()
 
 
@@ -25,15 +86,26 @@ def cycle36_test():
     tonic = 'C'
     sc = scale.MajorScale(tonic)
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'C5', 'B5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs2(sc, tonic_triad, "3/6")
     cycle.show()
+
+
+def cycle36_write_file_test():
+    tonic = 'C'
+    sc = scale.MajorScale(tonic)
+    tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'C5', 'B5'))
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
+    cycle.metadata = metadata.Metadata()
+    cycle.metadata.title = "Cycle 3/6 progression in C Major"
+    cycle.metadata.composer = "Chord Cycle Generation Software"
+    cycle.write("MusicXML", "C:\\Temp\\Cycle36_progression")
 
 
 def cycle45_test():
     tonic = 'C'
     sc = scale.MajorScale(tonic)
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'C5', 'B5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "4/5")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "4/5")
     cycle.show()
 
 
@@ -41,7 +113,7 @@ def cycle27_test():
     tonic = 'C'
     sc = scale.MajorScale(tonic)
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'C5', 'B5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "2/7")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "2/7")
     cycle.show()
 
 
@@ -50,14 +122,14 @@ def cycle36_arbitrary_scale_test():
     # the natural minor version
     sc = scale.ConcreteScale(pitches=['E4', 'F#4', 'G4', 'A4', 'B4', 'C#5', 'D#5'])
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'E5', 'D#6'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
     cycle.show()
 
 
 def cycle36_ab_test():
     sc = scale.MajorScale('Ab')
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'Ab4', 'G5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
     cycle.insert(0, key.KeySignature(-4))
     cycle.show()
 
@@ -65,7 +137,7 @@ def cycle36_ab_test():
 def cycle36_a_harmonic_minor_test():
     sc = scale.HarmonicMinorScale('A')
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'A4', 'G5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
     cycle.insert(0, key.KeySignature(0))
     cycle.show()
 
@@ -73,7 +145,7 @@ def cycle36_a_harmonic_minor_test():
 def cycle36_a_minor_test():
     sc = scale.MinorScale('A')
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'A4', 'G5'))
-    cycle = generate_cycle_pairs(tonic_triad, sc, "3/6")
+    cycle = generate_cycle_pairs(sc, tonic_triad, "3/6")
     cycle.insert(0, key.KeySignature(0))
     cycle.show()
 
@@ -98,12 +170,48 @@ def range_check_test():
     tonic_triad = chord.Chord(sc.pitchesFromScaleDegrees([1, 3, 5], 'C5', 'B5'))
     # create a string set to do the range checking with
     string_set = (GuitarRange.A_string, GuitarRange.D_string, GuitarRange.G_string)
-    cycle = generate_cycle_pairs(tonic_triad, sc, "2/7", string_set)
+    cycle = generate_cycle_pairs(sc, tonic_triad, "2/7", string_set)
     cycle.show()
 
 
+def generate_cycles_for_all_string_sets_test():
+    tonic = 'C'
+    root_scale = scale.MajorScale(tonic)
+    pair_type = '2/7'
+    generate_cycle_pairs_for_all_string_sets(root_scale, tonic, pair_type)
+
+
+def new_generate_sub_cycle_test():
+    # string_set = (GuitarRange.A_string, GuitarRange.D_string, GuitarRange.G_string)
+    tonic = 'C'
+    sc = scale.MajorScale(tonic)
+    root = sc.pitchFromDegree(1)
+    tonic_triad = chord.Chord([root, root.transpose(4), root.transpose(7)])
+    # sub_cycle = generate_sub_cycle(sc, tonic_triad, cycle2matrix[0], string_set)
+    sub_cycle, next_chord = generate_sub_cycle_2(sc, tonic_triad, cycle2matrix[0])
+    sub_cycle.append(next_chord)
+    sub_cycle.show()
+
+
+def new_generate_full_cycle_test():
+    tonic = 'C'
+    sc = scale.MajorScale(tonic)
+    root = sc.pitchFromDegree(1)
+    root.transpose(12, inPlace=True)
+    tonic_triad = chord.Chord([root, root.transpose(4), root.transpose(7)])
+
+    cycle, next_chord = generate_full_cycle_2(sc, tonic_triad, cycle3matrix)
+    m = stream.Measure()
+    m.append(next_chord)
+    cycle.append(m)
+    cycle.show()
+
+
+# check_note_ranges_test()
+# sub_cycle_test()
+# full_cycle_test()
 # cycle27_test()
-# cycle36_test()
+cycle36_test()
 # cycle45_test()
 # cycle36_drop3_test()
 # cycle36_drop2_test()
@@ -112,4 +220,8 @@ def range_check_test():
 # cycle36_a_minor_test()
 # cycle36_arbitrary_scale_test()
 # transpose_test()
-range_check_test()
+# range_check_test()
+# cycle36_write_file_test()
+# generate_cycles_for_all_string_sets_test()
+# new_generate_sub_cycle_test()
+# new_generate_full_cycle_test()
